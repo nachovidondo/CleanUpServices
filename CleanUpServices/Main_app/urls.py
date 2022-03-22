03ca545385fa9e6c343e95c90a9b5743b760809c
NachoVidondo <ignaciovidondo@hotmail.com>
2022-03-22 21:02:17 +0100

    Contact

--- CleanUpServices/Main_app/urls.py
+++ CleanUpServices/Main_app/urls.py
@@ -1,8 +1,10 @@
 
 from django.urls import path
-from .views import index , services
+from .views import index , services, contact, automatic
 
 urlpatterns = [
     path('', index, name ="index"),
-    path('services/', services, name ="services")
+    path('services/', services, name ="services"),
+    path('contact/', contact ,name ="contact"),
+    path('automatic/', automatic, name="automatic"),
 ]
--- CleanUpServices/Main_app/views.py
+++ CleanUpServices/Main_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Services, Happy_Customer
 
+from .forms  import Contactform
+from django.shortcuts import render,reverse , redirect
+from django.urls import reverse_lazy
+
 # Create your views here.
 
 def index(request):
@@ -12,3 +16,37 @@ def services(request):
     services = Services.objects.all()
     
     return render (request,'services.html', {'services':services })
+
+
+#Contact
+def contact(request):
+    contact_form = Contactform()
+  
+    if request.method == "POST":
+        contact_form = Contactform(data=request.POST)
+        if contact_form.is_valid():
+            name= request.POST.get('name')
+            email= request.POST.get('email')
+            content= request.POST.get('content')
+            """mail = EmailMessage(
+                "vida Message : New Message Contact ",
+                "From {} {}\n\n wrote :\n\n {}".format(name ,email,content),
+                "vida.com", ["vida90@gmail.com"],-----> create a new email
+                reply_to = [email]
+                )"""
+            try:
+               """ mail.send() #Si esta todo ok redireccionar"""
+               return redirect(reverse("automatic")+"?ok")
+
+
+
+            except:
+                return redirect(reverse("contact")+"?fail")
+
+    return render (request, 'contact.html', {'form':contact_form})
+
+
+#Automatic message after contact us and book us
+def automatic(request):
+  
+    return render (request, 'automatic.html')
